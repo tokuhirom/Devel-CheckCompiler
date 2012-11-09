4d8557ce9df88d342bb87764d72d6d2fe03c54e8
tokuhirom <tokuhirom@gmail.com>
2012-11-09 12:51:26 +0900

    initial import

---
+++ lib/Devel/CheckCompiler.pm
@@ -0,0 +1,39 @@
+package Devel::CheckCompiler;
+use strict;
+use warnings;
+use 5.010001;
+our $VERSION = '0.01';
+
+
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+Devel::CheckCompiler - ...
+
+=head1 SYNOPSIS
+
+  use Devel::CheckCompiler;
+
+=head1 DESCRIPTION
+
+Devel::CheckCompiler is
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno E<lt>tokuhirom AAJKLFJEF@ GMAIL COME<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+Copyright (C) Tokuhiro Matsuno
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
