eead8014c4d788840cd1e2f4ca1c7dae403cbf77
tokuhirom <tokuhirom@gmail.com>
2012-11-12 09:33:32 +0900

    Checking in changes prior to tagging of version 0.02.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index c487c73..94dabb1 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,4 +1,8 @@
     Revision history for Perl extension Devel::CheckCompiler
    
    +0.02
    +
    +    - docs
    +
     0.01    Fri Nov  9 12:51:23 2012
         - original version

--- lib/Devel/CheckCompiler.pm
+++ lib/Devel/CheckCompiler.pm
@@ -2,7 +2,7 @@ package Devel::CheckCompiler;
 use strict;
 use warnings;
 use 5.008001;
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 use parent qw/Exporter/;
 
 our @EXPORT = qw/check_c99 check_c99_or_exit/;
